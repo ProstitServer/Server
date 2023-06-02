5c5e8ff5e8aba81fd7afcb9aad21853ff822b930
Slava Petrov <slavapmk@gmail.com>
2023-06-02 23:12:13 +0300

    Patched auto updating start bat

--- start.bat
+++ start.bat
@@ -1,20 +1,17 @@
 @echo off
-chcp 65001
-cls
 
-set setup=true
-if exist git if exist jre set setup=false
-
-if "%setup%"=="true" (
-    echo Первая установка, может занять время
-    powershell -Command "Expand-Archive -Path git.zip -DestinationPath git -Force"
-    powershell -Command "Expand-Archive -Path jre.zip -DestinationPath jre -Force"
+if not exist git (
+    mkdir git
+    tar -C git -zxvf git.zip
+)
+if not exist jre (
+    mkdir jre
+    tar -C jre -zxvf jre.zip
 )
 
-echo Обновление файлов сервера
-git\bin\git pull
+git fetch --all
+git reset --hard origin/main
 
-echo Запуск сервера
 jre\bin\java -Xmx8g -Xms8g -jar paper-1.19.3-448.jar nogui
 
 pause
